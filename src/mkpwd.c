380767f3e4e954021664548ebde90072c93e1a6e
Nathan Gill <nathan.j.gill@outlook.com>
2025-03-02 19:46:27 +0000

    Added simple man page. Updates to mkpwd.

--- src/mkpwd.c
+++ src/mkpwd.c
@@ -20,7 +20,7 @@ const char* CHARSET_SPECIAL = "!\"#$%'()*+,-./:;<=>?@[]^_{|}~";
 unsigned int FAULTCODE = 0;
 
 void ERRORROUTINE(const char* msg, int errorCode) {
-	printf("%s\n", msg);
+	printf("%s. see \"man mkpwd\" for more information.\n", msg);
 	FAULTCODE = errorCode;
 	exit(FAULTCODE);
 }
@@ -36,9 +36,15 @@ int getTotalCharSetLen(bool* chars) {
 
 void compileCharSet(bool* chars) {
 	int charsetLen = getTotalCharSetLen(chars) + 1;
+
+	if (charsetLen <= 1) {
+		ERRORROUTINE("argument error", 1);
+		return;
+	}
+
 	CHARSET = (char*)malloc(charsetLen);
 	if (!CHARSET) {
-		ERRORROUTINE("memory allocation error", 1);
+		ERRORROUTINE("memory allocation error", 2);
 		return;
 	}
 
@@ -95,8 +101,9 @@ char* stripPrefixes(char* s) {
 void toLower(char* s) {
 	while (*s != '\0') {
 		if (*s >= 65 && *s <= 90) {
-			*s += 32;
+			(*s) += 32;
 		}
+		s++;
 	}
 }
 
@@ -115,6 +122,63 @@ int getArgNumber(int argc, char* argv[], int* i) {
 	return 0;
 }
 
+void showHelp() {
+	printf("mkpwd - generate secure passwords\n");
+	printf("version 1.0\n\n");
+	printf("USAGE\n");
+	printf("	mkpwd [OPTIONS]\n\n");
+	printf("OPTIONS\n");
+	printf("	-n XX, --number XX\n");
+	printf("		Specify the number of passwords to generate (default: 1, or environment variable).\n\n");
+	printf("	-l XX, --length XX\n");
+	printf("		Set the length of each password (default: 18, or environment variable).\n\n");
+	printf("	-h, --help\n");
+	printf("		Display this help message and exit.\n\n");
+	printf("	+lusn");
+	printf("		Specify character sets to include:\n");
+	printf("		  l  Lowercase letters\n");
+	printf("		  u  Uppercase letters\n");
+	printf("		  s  Symbols\n");
+	printf("		  n  Numbers\n");
+	printf("		Default: +lun if not specified (or environment variable).\n\n");
+	printf("AUTHOR\n");
+	printf("	Written by Nathan Gill.\n");
+	printf("	Licensed under the BSD 2-Clause \"Simplified\" License.\n");
+	printf("	See \"LICENSE.txt\" for more information.\n\n");
+	printf("Run \"man mkpwd\" for examples, environment variables, and extended help.\n");
+}
+
+bool parseCharArguments(bool* chars, char* args) {
+	char* stripped = stripPrefixes(args);
+	bool chars_set = false;
+	for (int j = 0; j < strlen(stripped); j++) {
+		switch (stripped[j]) {
+			case 'l':
+				chars[0] = true;
+				chars_set = true;
+				break;
+			case 'u':
+				chars[1] = true;
+				chars_set = true;
+				break;
+			case 'n':
+				chars[2] = true;
+				chars_set = true;
+				break;
+			case 's':
+				chars[3] = true;
+				chars_set = true;
+				break;
+			case '+':
+				break;
+			default:
+				ERRORROUTINE("argument error", 1);
+				break;
+			}
+		}
+	return chars_set;
+}
+
 void fillFromArgs(PasswordOptions* options, int argc, char* argv[]) {
 	bool n_set = false;
 	bool length_set = false;
@@ -123,7 +187,7 @@ void fillFromArgs(PasswordOptions* options, int argc, char* argv[]) {
 	bool custom_chars[4] = { false, false, false, false };
 
 	for (int i = 1; i < argc; i++) {
-		//toLower(argv[i]);
+		toLower(argv[i]);
 		char* stripped = stripPrefixes(argv[i]);
 
 #ifdef DEBUG
@@ -138,30 +202,12 @@ void fillFromArgs(PasswordOptions* options, int argc, char* argv[]) {
 			options->length = getArgNumber(argc, argv, &i);
 			length_set = true;
 		}
+		else if (!strcmp(stripped, "h") || !strcmp(stripped, "help")) {
+			showHelp();
+			exit(0);
+		}
 		else if (stripped[0] == '+') {
-			for (int j = 1; j < strlen(stripped); j++) {
-				switch (stripped[j]) {
-					case 'l':
-						custom_chars[0] = true;
-						chars_set = true;
-						break;
-					case 'u':
-						custom_chars[1] = true;
-						chars_set = true;
-						break;
-					case 'n':
-						custom_chars[2] = true;
-						chars_set = true;
-						break;
-					case 's':
-						custom_chars[3] = true;
-						chars_set = true;
-						break;
-					default:
-						ERRORROUTINE("argument error", 1);
-						break;
-				}
-			}
+                        chars_set = parseCharArguments(&(custom_chars[0]), stripped);
 		}
 	}
 
@@ -201,11 +247,12 @@ void searchEnvVars() {
 	char* envPasswordChars = getenv("MKPWD_DEF_PASSWORDCHARS");
 	if (envPasswordChars) {
 		char* stripped = stripPrefixes(envPasswordChars);
-		if (strcmp(stripped, "") != 0 && strlen(stripped) == 4) {
-			DEF_PASSWORDCHARS[0] = (stripped[0] == '1') ? true : false;
-			DEF_PASSWORDCHARS[1] = (stripped[1] == '1') ? true : false;
-			DEF_PASSWORDCHARS[2] = (stripped[2] == '1') ? true : false;
-			DEF_PASSWORDCHARS[3] = (stripped[3] == '1') ? true : false;
+		if (strcmp(stripped, "") != 0) {
+		        DEF_PASSWORDCHARS[0] = false;
+		        DEF_PASSWORDCHARS[1] = false;
+		        DEF_PASSWORDCHARS[2] = false;
+		        DEF_PASSWORDCHARS[3] = false;
+                        parseCharArguments(&(DEF_PASSWORDCHARS[0]), stripped);
 		}
 	}
 }
